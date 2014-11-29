79939ac5bdb3936bbfbd978cc86448bf1a7c757f
salah <salahakanda85@gmail.com>
2014-11-29 14:56:46 -0500

    modify1

--- src/primeNumber/Math.java
+++ src/primeNumber/Math.java
@@ -14,13 +14,20 @@ public class Math {
 //                System.out.println( N );
 //        }
 
-
-        int init =1, pos =1;
-        int x = 100;
-        for(int i =1; i<=x;i++){
-            System.out.println(init);
-            init += pos;
-            pos++;
+//
+//        int init =1, pos =1;
+//        int x = 100;
+//        for(int i =1; i<=x;i++){
+//            System.out.println(init);
+//            init += pos;
+//            pos++;
+//        }
+        int init1 =1, pos1 =1;
+        int y = 5;
+        for(int i =1; i<=y;i++){
+            System.out.println(init1);
+            init1 += (pos1 + 1);
+            pos1++;
         }
 
     }
