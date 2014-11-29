6ee356f420a16d2262d416779176db96e8a30841
salah <salahakanda85@gmail.com>
2014-11-29 14:49:27 -0500

    mocify

---
+++ src/primeNumber/Math.java
@@ -0,0 +1,27 @@
+package primeNumber;
+
+
+public class Math {
+    public static void main( String[] args){
+//        for (int N = 1; N <= 10; N++) {
+//            System.out.println( 2*N );
+//        }
+//        for (int N = 2; N <= 20; N = N + 2) {
+//            System.out.println( N );
+//        }
+//        for (int N = 2; N <= 20; N++) {
+//            if ( N % 2 == 0 ) // is N even?
+//                System.out.println( N );
+//        }
+
+
+        int init =1, pos =1;
+        int x = 100;
+        for(int i =1; i<=x;i++){
+            System.out.println(init);
+            init += pos;
+            pos++;
+        }
+
+    }
+}
